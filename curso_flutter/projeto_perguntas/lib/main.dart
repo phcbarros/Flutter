748d49f8e7c984997ab3ff3f3417c7ec0d55c4ce
phcbarros <paulo.barros@outlook.com.br>
2021-03-29 18:04:46 -0300

    Curso Flutter - 02 Noções Básicas de Flutter - 31 Usando Column parar Organizar os Widgets

--- curso_flutter/projeto_perguntas/lib/main.dart
+++ curso_flutter/projeto_perguntas/lib/main.dart
@@ -4,15 +4,35 @@ main() => runApp(PerguntaApp());
 
 // nó raiz da aplicação PerguntaApp
 class PerguntaApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual seu animal favorito?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Text('Olá Flutter!!!'),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            )
+          ],
+        ),
       ),
     );
   }
