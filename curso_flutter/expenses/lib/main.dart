244c27007b46b1fffaa2417754474ae8843c2dee
phcbarros <paulo.barros@outlook.com.br>
2021-03-30 18:03:03 -0300

    Curso Flutter - 04 Construindo App Real (Despesas Pessoais) - 80 Criando Classe de Modelo (Transaction)

--- curso_flutter/expenses/lib/main.dart
+++ curso_flutter/expenses/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() {
@@ -14,26 +15,29 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(id: '1', title: 'Box Pokemon', value: 199.0, date: DateTime.now()),
+    Transaction(id: '2', title: 'Conta de Luz', value: 100.0, date: DateTime.now()),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Despesas Pessoais"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            child: Card(
-              child: Text("Gráfico"),
+        appBar: AppBar(
+          title: Text("Despesas Pessoais"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Card(
+                child: Text("Gráfico"),
+              ),
             ),
-          ),
-          Card(
-            child: Text("Lista de transações"),
-          )
-        ],
-      )
-    );
+            Card(
+              child: Text("Lista de transações"),
+            )
+          ],
+        ));
   }
 }
