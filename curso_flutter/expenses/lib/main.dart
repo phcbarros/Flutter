659b03d09f32b0dc3ea4110fbd99ce93e745438c
phcbarros <paulo.barros@outlook.com.br>
2021-03-30 19:18:12 -0300

    Curso Flutter - 04 Construindo App Real (Despesas Pessoais) - 92 Usando TextField #02

--- curso_flutter/expenses/lib/main.dart
+++ curso_flutter/expenses/lib/main.dart
@@ -16,12 +16,16 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
   final _transactions = [
     Transaction(
         id: '1', title: 'Box Pokemon', value: 199.00, date: DateTime.now()),
     Transaction(
         id: '2', title: 'Conta de Luz', value: 100.55, date: DateTime.now()),
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,19 +43,16 @@ class MyHomePage extends StatelessWidget {
             Column(
               children: _transactions.map((tr) {
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10
-                        ),
+                    child: Row(
+                  children: [
+                    Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                         decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          )
-                        ),
+                            border: Border.all(
+                          color: Colors.purple,
+                          width: 2,
+                        )),
                         padding: EdgeInsets.all(10),
                         child: Text(
                           "R\$ ${tr.value.toStringAsFixed(2)}",
@@ -59,29 +60,24 @@ class MyHomePage extends StatelessWidget {
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
                             color: Colors.purple,
-                          ),)
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            tr.title,
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16
-                            ),
                           ),
-                          Text(
-                            DateFormat('d MMM y').format(tr.date),
-                            style: TextStyle(
-                              color: Colors.grey
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  )
-                );
+                        )),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        Text(
+                          DateFormat('d MMM y').format(tr.date),
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    )
+                  ],
+                ));
               }).toList(),
             ),
             Card(
@@ -90,24 +86,25 @@ class MyHomePage extends StatelessWidget {
                 child: Column(
                   children: [
                     TextField(
-                      decoration: InputDecoration(
-                        labelText: "Título"
-                      ),
+                      controller: titleController,
+                      decoration: InputDecoration(labelText: "Título"),
                     ),
                     TextField(
-                      decoration: InputDecoration(
-                        labelText: "Valor (R\$)"
-                      ),
+                      controller: valueController,
+                      decoration: InputDecoration(labelText: "Valor (R\$)"),
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         TextButton(
-                          onPressed: () {}, 
+                          onPressed: () {
+                            print(titleController.text);
+                            print(valueController.text);
+                          },
                           child: Text("Nova Transação"),
                           style: ButtonStyle(
-                            foregroundColor: MaterialStateProperty.all(Colors.purple)
-                          ),
+                              foregroundColor:
+                                  MaterialStateProperty.all(Colors.purple)),
                         ),
                       ],
                     )
