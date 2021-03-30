e321bfcf3cfd0dd083a85b69e36063e9f7a67c5c
phcbarros <paulo.barros@outlook.com.br>
2021-03-30 19:41:10 -0300

    Curso Flutter - 04 Construindo App Real (Despesas Pessoais) - 93 Dividindo App em Widgets

--- curso_flutter/expenses/lib/main.dart
+++ curso_flutter/expenses/lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import './components/transaction_user.dart';
 
 main() {
   runApp(ExpensesApp());
@@ -16,15 +15,6 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
-  final _transactions = [
-    Transaction(
-        id: '1', title: 'Box Pokemon', value: 199.00, date: DateTime.now()),
-    Transaction(
-        id: '2', title: 'Conta de Luz', value: 100.55, date: DateTime.now()),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -40,78 +30,7 @@ class MyHomePage extends StatelessWidget {
                 child: Text("Gráfico"),
               ),
             ),
-            Column(
-              children: _transactions.map((tr) {
-                return Card(
-                    child: Row(
-                  children: [
-                    Container(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                        decoration: BoxDecoration(
-                            border: Border.all(
-                          color: Colors.purple,
-                          width: 2,
-                        )),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          "R\$ ${tr.value.toStringAsFixed(2)}",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple,
-                          ),
-                        )),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tr.title,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 16),
-                        ),
-                        Text(
-                          DateFormat('d MMM y').format(tr.date),
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ],
-                    )
-                  ],
-                ));
-              }).toList(),
-            ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: titleController,
-                      decoration: InputDecoration(labelText: "Título"),
-                    ),
-                    TextField(
-                      controller: valueController,
-                      decoration: InputDecoration(labelText: "Valor (R\$)"),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            print(titleController.text);
-                            print(valueController.text);
-                          },
-                          child: Text("Nova Transação"),
-                          style: ButtonStyle(
-                              foregroundColor:
-                                  MaterialStateProperty.all(Colors.purple)),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            )
+            TransactionUser()
           ],
         ));
   }
