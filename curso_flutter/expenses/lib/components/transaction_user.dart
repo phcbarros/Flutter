7e6b0b0ec1d558a2f4ef1961613b4e919eb6d1a8
phcbarros <paulo.barros@outlook.com.br>
2021-03-31 14:27:29 -0300

    Curso Flutter - 04 Construindo App Real (Despesas Pessoais) - 94 Adicionando Transação #01

--- curso_flutter/expenses/lib/components/transaction_user.dart
+++ curso_flutter/expenses/lib/components/transaction_user.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:expenses/models/transaction.dart';
 import 'package:expenses/components/transaction_form.dart';
@@ -9,11 +11,24 @@ class TransactionUser extends StatefulWidget {
 }
 
 class _TransactionUserState extends State<TransactionUser> {
-
- final _transactions = [
-    Transaction(id: '1', title: 'Box Pokemon', value: 199.00, date: DateTime.now()),
-    Transaction(id: '2', title: 'Conta de Luz', value: 100.55, date: DateTime.now()),
+  final _transactions = [
+    Transaction(
+        id: '1', title: 'Box Pokemon', value: 199.00, date: DateTime.now()),
+    Transaction(
+        id: '2', title: 'Conta de Luz', value: 100.55, date: DateTime.now()),
   ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+        id: Random().nextDouble().toString(),
+        title: title,
+        value: value,
+        date: DateTime.now());
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
